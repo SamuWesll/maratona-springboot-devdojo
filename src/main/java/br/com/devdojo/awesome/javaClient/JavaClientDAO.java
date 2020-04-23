51036854487a2c29500fbeb9e052bf8c80408879
Samuel Weslley M. Barboza <SWBarboza@raiadrogasil.com>
2020-04-23 10:30:26 -0300

    Spring Boot Essentials 23 - Cliente Spring com RestTemplate pt 03 POST

---
+++ src/main/java/br/com/devdojo/awesome/javaClient/JavaClientDAO.java
@@ -0,0 +1,50 @@
+package br.com.devdojo.awesome.javaClient;
+
+import br.com.devdojo.awesome.model.Students;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class JavaClientDAO {
+
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/protected/students")
+            .basicAuthentication("samuel","123").build();
+
+    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/admin/students")
+            .basicAuthentication("admin","123").build();
+
+    public Students findById(long id) {
+        Students student = restTemplate.getForObject("/{id}", Students.class, 1);
+//        ResponseEntity<Students> forEntity = restTemplate.getForEntity("/{id}", Students.class, 1);
+        return student;
+    };
+
+    public List<Students> listAll() {
+        ResponseEntity<List<Students>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Students>>() {
+                });
+        return exchange.getBody();
+    };
+
+    public Students save(Students studentsPost) {
+//        ResponseEntity<Students> exchangePost = restTemplateAdmin.exchange("/",
+//                HttpMethod.POST,new HttpEntity<>(studentsPost, createJSONHeader()), Students.class);
+        Students studentsPostObject = restTemplateAdmin.postForObject("/", studentsPost, Students.class);
+        return studentsPostObject;
+    }
+
+    private static HttpHeaders createJSONHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+}
--- src/main/java/br/com/devdojo/awesome/javaClient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/awesome/javaClient/JavaSpringClientTest.java
@@ -3,8 +3,7 @@ package br.com.devdojo.awesome.javaClient;
 import br.com.devdojo.awesome.model.Students;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -13,19 +12,13 @@ import java.util.List;
 public class JavaSpringClientTest {
 
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthentication("samuel","123").build();
-        Students student = restTemplate.getForObject("/{id}", Students.class, 1);
-        ResponseEntity<Students> forEntity = restTemplate.getForEntity("/{id}", Students.class, 1);
-//        System.out.println(students);
-        System.out.println(forEntity.getBody());
-//        Students[] students = restTemplate.getForObject("/", Students[].class);
-//        System.out.println(Arrays.toString(students));
-        ResponseEntity<List<Students>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Students>>() {
-        });
-        System.out.println(exchange.getBody());
+
+        Students studentsPost = new Students();
+        studentsPost.setName("Alice Gata");
+        studentsPost.setEmail("alice@rocha.com");
+        JavaClientDAO dao = new JavaClientDAO();
+
+        System.out.println(dao.save(studentsPost));
     }
 
 }
